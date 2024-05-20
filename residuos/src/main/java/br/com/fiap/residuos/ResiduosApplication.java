f09446f7514c90a8ee1bc1d49dc5f2fd5ad66e8c
Odamiani <feldamiani@gmail.com>
2024-05-20 18:44:37 -0300

    start spring

---
+++ residuos/src/main/java/br/com/fiap/residuos/ResiduosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.residuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiduosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiduosApplication.class, args);
+	}
+
+}
---
+++ residuos/src/test/java/br/com/fiap/residuos/ResiduosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.residuos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiduosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
